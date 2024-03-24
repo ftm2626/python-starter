fc56602f4d6a1bb98251e326a11d48061446a115
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2024-03-24 11:54:22 +0330

    variables and type casting

---
+++ 1.basics/1.variables.py
@@ -0,0 +1,22 @@
+# string
+name = "jasmine"
+
+# int
+age = 25
+age += 1 
+# float
+performance = 99.89
+
+# type casting
+print("my age is :" + str(age))
+
+# boolean
+human = True
+
+# multiple assignment
+# allows us to assing multiple variables at the same time in one line of code
+name, age, attractive = "Bro", 21, True
+
+# all of the below variables equal 30
+fatemeh = zahra = mehdi = ali = 30
+print(fatemeh)
---
+++ 1.basics/2.buildInMethods.py
@@ -0,0 +1,20 @@
+# string
+
+name = "jasMine"
+digit_name = "235689"
+
+length = len(name)
+find_index = name.find("a")
+cap = name.capitalize()
+uppercase =name.upper()
+lowercase = name.lower()
+is_digit = digit_name.isdigit() # True
+is_alphabet = digit_name.isalpha() # False
+count_how_many_chars = name.count("e") #1
+jasmine_with_u = name.replace("e","u")
+display_three_times = name * 3
+
+print(display_three_times)
+
+
+# 00:30:00
