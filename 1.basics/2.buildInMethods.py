62cae7631fab64d541dbe087bfffbb77aa98b9b6
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2024-03-25 15:44:50 +0330

    input, if, logical ops

--- 1.basics/2.buildInMethods.py
+++ 1.basics/2.buildInMethods.py
@@ -1,7 +1,9 @@
-# string
+import math;
 
+
+# ----------------------STRING----------------------
 name = "jasMine"
-digit_name = "235689"
+digit_name = "235689" 
 
 length = len(name)
 find_index = name.find("a")
@@ -14,7 +16,32 @@ count_how_many_chars = name.count("e") #1
 jasmine_with_u = name.replace("e","u")
 display_three_times = name * 3
 
-print(display_three_times)
+# slicing : create a substring by extracting elements from another string
+#       indexing[] : [start:stop:step]
+full_name = "jasmine hunt"
+second_letter = full_name[1]
+first_name = full_name[0:7] # or full_name[:7]
+weird_name = full_name[::2]
+reverse_name = full_name[::-1]
+#       slice()
+website = "http://google.com"
+website_name_index = slice(7,-4) #second params is nagative index that counts from right
+website_name = website[website_name_index]
 
 
-# 00:30:00
+
+
+# ------------------------NUMBERS-------------------
+# import math;
+pi = 3.14
+round_num = round(pi)
+ceil_num = math.ceil(pi)
+floor_num = math.floor(pi)
+abs_num = abs(pi)
+power_num = pow(pi,3)
+squire_num = math.sqrt(420)
+max_num = max(1,2,3,4)
+min_num = min(1,2,3,4)
+
+
+print(website_name)
---
+++ 4.if-logicalOprators.py
@@ -0,0 +1,18 @@
+age = int(input('how old are you? '))
+
+if age == 100:
+    print("jesus!!!")
+elif age >=18:
+    print("how's life?")
+else:
+    print("goodluck kid!")
+
+height = int(input("how tall are you? "))
+
+if height>=160 and height<= 190:
+    print("congrates! you won the genetic lotery! ")
+elif height<160 or height>190:
+    print('damn! not lucky i guess') 
+
+
+# 1:04:00
